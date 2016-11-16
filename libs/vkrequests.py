039f2ef5566264f36b19de843601cd8a4b5facad
Eugene <gish.ee18@gmail.com>
2016-11-16 18:07:57 +0300

    рефакторинг

--- libs/vkrequests.py
+++ libs/vkrequests.py
@@ -9,15 +9,17 @@ from libs import vk
 __author__ = 'Eugene Ershov - http://vk.com/fogapod'
 
 GROUP_ID = '99411738'
+MGROUP_ID = '-99411738'
 
 api = None
 
 
-def vk_request_errors(REQUEST):
+def vk_request_errors(request):
     def request_errors(*args, **kwargs):
-        # RESPONSE = REQUEST(*args, **kwargs)  # Для вывода ошибки в консоль
+        # response = request(*args, **kwargs); time.sleep(0.66) 
+        # Для вывода ошибки в консоль
         try:
-            RESPONSE = REQUEST(*args, **kwargs)
+            response = request(*args, **kwargs)
         except Exception as e:
             if 'Too many requests per second.' in str(e):
                 time.sleep(0.66)
@@ -37,7 +39,7 @@ def vk_request_errors(REQUEST):
                     print('\nERROR! ' + str(e) + '\n')
             return False, str(e)
         else:
-            return RESPONSE, True
+            return response, True
     return request_errors
 
 
@@ -45,7 +47,7 @@ def vk_request_errors(REQUEST):
 def log_in(**kwargs):
     # TODO: получить токен.
     """
-    args: LOGIN ( required ), PASSWORD ( required )
+    kwargs: login ( required ), password ( required )
     returns True if succeed
     else returns False
     """
@@ -54,6 +56,7 @@ def log_in(**kwargs):
     # 65536 -- offline permission; 8192 -- wall permission; 131072 -- docs
     # permission; 4 -- photos permission
     app_id = '5720412'
+
     if 'token' in kwargs:
         token = kwargs['token']
         session = vk.Session(
@@ -65,185 +68,181 @@ def log_in(**kwargs):
             user_login=login, user_password=password,
             scope=scope, app_id=app_id
         )
+
     api = vk.API(session, v='5.6')
     api.stats.trackVisitor()
+
     return True
 
 
 @vk_request_errors
-def get_members_count(*args):
+def get_members_count():
     """
-    args: None
     returns string if succeed
     else returns False
     """
-
-    RESPONSE = api.groups.getById(
-        group_id=GROUP_ID,
-        fields='members_count')
-    return RESPONSE[0]['members_count']
+    response = api.groups.getById(group_id=GROUP_ID, fields='members_count')
+    return response[0]['members_count']
 
 
 @vk_request_errors
 def get_issues(*args):
     """
-    args: OFFSET ( required, but not throws an exception if not declared,
-    default=() ), POST_COUNT ( optional, default='30' )
+    args: offset ( required, but not throws an exception if not declared,
+    default=() ), post_count ( optional, default='30' )
     returns dict with wall posts if succeed
     else returns False
     """
-
     if len(args) == 2:
-        OFFSET, POST_COUNT = args
+        offset, post_count = args
     else:
-        OFFSET = args
-        POST_COUNT = '30'
-    # запись со смайликами: 23 (offset='22')
+        offset = args
+        post_count = '30'
+
     return api.wall.get(
-        owner_id='-' + GROUP_ID, filter='others', extended='1',
-        offset=OFFSET, count=POST_COUNT
+        owner_id=MGROUP_ID, filter='others', extended='1',
+        offset=offset, count=post_count
     )
 
 
 @vk_request_errors
-def get_issue_count(*args):
+def get_issue_count():
     return api.execute.GetIssueCount()
 
 
 @vk_request_errors
 def send_issue(*args):
     """
-    args: ISSUE_DATA {'file','image','theme','issue'}
+    args: issue_data {'file','image','theme','issue'}
 
     returns string ( post id ) if succeed
     else returns False
     """
-    ISSUE_DATA = args[0]
-    FILE = ISSUE_DATA['file']
-    IMAGE = ISSUE_DATA['image']
-    THEME = ISSUE_DATA['theme']
-    ISSUE = ISSUE_DATA['issue']
+    issue_data = args[0]
+    file_path = issue_data['file']
+    image_path = issue_data['image']
+    theme_text = issue_data['theme']
+    issue_text = issue_data['issue']
 
-    ATTACHMENTS = []
+    attachments = []
 
-    DOC = attach_doc(FILE)[0]
-    PIC = attach_pic(IMAGE)[0]
+    doc = attach_doc(file_path)[0]
+    pic = attach_pic(image_path)[0]
 
-    if DOC:
-        ATTACHMENTS.append('doc' +
-                           str(DOC[0]['owner_id']) +
-                           '_' +
-                           str(DOC[0]['id']))
-    if PIC:
-        ATTACHMENTS.append('photo' +
-                           str(PIC[0]['owner_id']) +
-                           '_' +
-                           str(PIC[0]['id']))
+    if doc:
+        attachments.append('doc' + str(doc[0]['owner_id'])
+                           + '_' + str(doc[0]['id'])
+                           )
+    if pic:
+        attachments.append('photo' + str(pic[0]['owner_id'])
+                           + '_' + str(pic[0]['id'])
+                           )
 
     return api.wall.post(
-        owner_id='-' + GROUP_ID, message=THEME + '\n\n' + ISSUE,
-        attachments=ATTACHMENTS
+        owner_id=MGROUP_ID, message=theme_text
+        + '\n\n' + issue_text, attachments=attachments
     )
 
 
 @vk_request_errors
 def get_comments(*args):
     """
-    args: POST_ID ( requied ), OFFSET ( required, but not throws an exception
-    if not declared, default=() ), COMMENT_COUNT ( optional, default='100' )
+    args: post_id ( requied ), offset ( required, but not throws an exception
+    if not declared, default=() ), comment_count ( optional, default='100' )
     returns dict with comments if succeed else returns False
     """
-
     if len(args) == 3:
-        POST_ID, OFFSET, COMMENT_COUNT = args
+        post_id, offset, comment_count = args
     else:
-        POST_ID, OFFSET = args
-        COMMENT_COUNT = '100'
+        post_id, offset = args
+        comment_count = '100'
+
     return api.wall.getComments(
-        owner_id='-' + GROUP_ID, post_id=POST_ID, offset=OFFSET,
-        count=COMMENT_COUNT
+        owner_id=MGROUP_ID, post_id=post_id,
+        offset=offset, count=comment_count
     )
 
 
 @vk_request_errors
-def get_user_name(*args):
+def get_user_name():
     """
-    args: None
     returns string (First_name Last_name) if succeed
     else returns False
     """
-    RESPONSE = api.users.get()[0]
-    return RESPONSE['first_name'] + ' ' + RESPONSE['last_name']
+    response = api.users.get()[0]
+
+    return response['first_name'] + ' ' + response['last_name']
 
 
 @vk_request_errors
 def get_user_photo(*args):
     """
-    args: PHOTO_SIZE ( optional ), default = 'photo_big', can be:
+    args: photo_size ( optional ), default = 'photo_big', can be:
     'photo_medium', 'photo_small', 'photo_max' (super tiny photo)
+
     returns Photo if succeed
     # returns None if user have no avatar
     else returns False
     """
-
     if len(args) == 1:
-        PHOTO_SIZE = args[0]
+        photo_size = args[0]
     else:
-        PHOTO_SIZE = 'photo_big'
-    url = api.users.get(fields=PHOTO_SIZE)[0]
+        photo_size = 'photo_big'
+    url = api.users.get(fields=photo_size)[0]
+
     # !always returns photo!
-    if 'images/question_c.gif' not in url[PHOTO_SIZE]:
-        return r.get(url[PHOTO_SIZE]).content
+    if 'images/question_c.gif' not in url[photo_size]:
+        return r.get(url[photo_size]).content
 
-
-# processing ATTACHMENTS in send_issue
-# WIP
 
 @vk_request_errors
 def attach_doc(*args):
     """
-    args: PATH ( required )
+    args: path ( required )
 
     returns array with doc object
     else returns False
     """
-    PATH = args[0]
+    path = args[0]
 
-    if PATH:
-        UPLOAD_DATA = api.docs.getUploadServer()
-        DOC = {'file': open(PATH, 'rb')}
-        UPLOADED = r.post(UPLOAD_DATA['upload_url'], files=DOC)
-        JSON_DATA = UPLOADED.json()
+    if path:
+        upload_data = api.docs.getUploadServer()
 
-        if 'error' in JSON_DATA:
+        doc = {'file': open(path, 'rb')}
+
+        response = r.post(upload_data['upload_url'], files=doc)
+        json_data = response.json()
+
+        if 'error' in json_data:
             raise Exception('Failed loading document')
 
-        return api.docs.save(
-            title=re.match('/.+$', PATH), file=JSON_DATA['file']
+        return api.docs.save(title=re.match(
+            '/.+$', path), file=json_data['file']
         )
 
 
 @vk_request_errors
 def attach_pic(*args):
     """
-    args: PATH ( required )
+    args: path ( required )
 
     returns array with picture object
     else returns False
     """
-    PATH = args[0]
+    path = args[0]
 
-    if PATH:
-        UPLOAD_DATA = api.photos.getWallUploadServer(
-            group_id=GROUP_ID)
-        PHOTO = {'photo': open(PATH, 'rb')}
-        UPLOADED = r.post(UPLOAD_DATA['upload_url'], files=PHOTO)
-        JSON_DATA = UPLOADED.json()
+    if path:
+        upload_data = api.photos.getWallUploadServer(group_id=GROUP_ID)
 
-        if JSON_DATA['photo'] == '[]':
+        pic = {'photo': open(path, 'rb')}
+
+        response = r.post(upload_data['upload_url'], files=pic)
+        json_data = response.json()
+
+        if json_data['photo'] == '[]':
             raise Exception('Failed loading picture')
 
         return api.photos.saveWallPhoto(
-            group_id=GROUP_ID, photo=JSON_DATA['photo'],
-            server=JSON_DATA['server'], hash=JSON_DATA['hash']
+            group_id=GROUP_ID, photo=json_data['photo'],
+            server=json_data['server'], hash=json_data['hash']
         )
