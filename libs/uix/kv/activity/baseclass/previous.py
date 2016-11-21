466d04003d478d2815841fc4dd7213fae1ad32f2
CityDoomed <gorodage@gmail.com>
2016-11-21 19:37:27 +0300

    Теперь диалог восстановления формы выводится при попытке открыть форму ввода вопроса

--- libs/uix/kv/activity/baseclass/previous.py
+++ libs/uix/kv/activity/baseclass/previous.py
@@ -9,13 +9,18 @@ class Previous(Screen):
     def on_enter(self):
         '''Вызывается при установке Activity на экран.'''
 
-        if self.manager._app:
-            self.manager._app.screen.ids.action_bar.left_action_items = \
-                [['menu', lambda x: self.manager._app.nav_drawer.toggle()]]
-            self.manager._app.screen.ids.action_bar.title = \
-                self.manager._app.title
-            Clock.schedule_interval(self.manager._app.set_banner, 8)
+        app = self.manager._app
+        if app:
+            app.screen.ids.action_bar.left_action_items = \
+                [['menu', lambda x: app.nav_drawer.toggle()]]
+            app.screen.ids.action_bar.title = app.title
+            Clock.schedule_interval(app.set_banner, 8)
 
     def on_button_question(self, *args):
-        Clock.unschedule(self.manager._app.set_banner)
-        self.manager._app.screen.ids.manager.current = 'ask a question'
+        app = self.manager._app
+        Clock.unschedule(app.set_banner)
+
+        if app.saved_form:
+            app.dialog_restore_form()
+        else:
+            self.manager.current = 'ask a question'
