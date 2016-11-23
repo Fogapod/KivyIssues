2615181b518b7e5c1398b4e361c2576006d71884
CityDoomed <gorodage@gmail.com>
2016-11-23 04:31:10 +0300

    Исправлена ошибка смены Activity в списке постов

--- program.py
+++ program.py
@@ -314,11 +314,19 @@ class Program(App, _class.ShowPlugin, _class.ShowAbout, _class.ShowLicense,
         if name_screen in (1001, 27):
             if name_current_screen == 'previous':
                 self.dialog_exit()
-                return
+            return
 
-        self.manager.current = 'previous' \
-            if name_current_screen == 'ask a question' \
-            else name_screen
+        if name_current_screen == 'ask a question' \
+                or name_screen in (1001, 27):
+            self.manager.current = 'previous'
+        elif name_current_screen == 'box posts' \
+                or name_screen in (1001, 27):
+            if name_screen in (1001, 27):
+                self.manager.current = self.screen.ids.box_posts.old_screen
+            else:
+                self.manager.current = name_screen
+        else:
+            self.manager.current = name_screen
 
     def dialog_fill_out_form(self):
         '''Диалог с предложением сохранить форму вопроса.'''
