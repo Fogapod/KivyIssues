694955a838b653dec62f140e1e9871ff4418f15c
CityDoomed <gorodage@gmail.com>
2016-12-29 01:15:09 +0300

    Add method 'clear'

--- libs/uix/kv/activity/baseclass/form_input_text.py
+++ libs/uix/kv/activity/baseclass/form_input_text.py
@@ -3,4 +3,8 @@ from kivy.properties import ObjectProperty
 
 
 class FormInputText(BoxLayout):
-    callback = ObjectProperty(lambda x: None)
+    callback = ObjectProperty(lambda x: x)
+
+    def clear(self):
+        self.ids.text_input.text = ''
+        self.ids.text_input.message = ''
