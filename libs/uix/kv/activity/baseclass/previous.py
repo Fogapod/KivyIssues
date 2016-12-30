b7665dcc90c569cd8fc1c25edf2334a178846332
CityDoomed <gorodage@gmail.com>
2016-12-30 06:47:05 +0300

    Удален метод on_button_questions

--- libs/uix/kv/activity/baseclass/previous.py
+++ libs/uix/kv/activity/baseclass/previous.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from kivy.uix.screenmanager import Screen
-from kivy.clock import Clock
 
 
 class Previous(Screen):
@@ -14,13 +13,3 @@ class Previous(Screen):
             app.screen.ids.action_bar.left_action_items = \
                 [['menu', lambda x: app.nav_drawer.toggle()]]
             app.screen.ids.action_bar.title = app.title
-            Clock.schedule_interval(app.set_banner, 8)
-
-    def on_button_question(self, *args):
-        app = self.manager._app
-        Clock.unschedule(app.set_banner)
-
-        if app.saved_form:
-            app.dialog_restore_form()
-        else:
-            self.manager.current = 'ask a question'
