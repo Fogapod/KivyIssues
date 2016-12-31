5db6f59686dba5b4aea7e04b5a7a1bf519e61324
CityDoomed <gorodage@gmail.com>
2016-12-31 13:32:58 +0300

    Создан метод для обработки событий формы ввода текста на главном экране

--- libs/uix/kv/activity/baseclass/previous.py
+++ libs/uix/kv/activity/baseclass/previous.py
@@ -1,15 +1,31 @@
 # -*- coding: utf-8 -*-
 
 from kivy.uix.screenmanager import Screen
+from kivy.properties import ObjectProperty
+
+from libs.vkrequests import create_issue
 
 
 class Previous(Screen):
+    
+    _app = ObjectProperty()
 
     def on_enter(self):
         '''Вызывается при установке Activity на экран.'''
 
-        app = self.manager._app
-        if app:
-            app.screen.ids.action_bar.left_action_items = \
-                [['menu', lambda x: app.nav_drawer.toggle()]]
-            app.screen.ids.action_bar.title = app.title
+        self._app.screen.ids.action_bar.left_action_items = \
+            [['menu', lambda x: self._app.nav_drawer.toggle()]]
+        self._app.screen.ids.action_bar.title = self._app.title
+
+        self.ids.input_text_form.callback = self.callback_for_input_text
+
+    def callback_for_input_text(self, flag):
+        if flag in ('FILE', 'FOTO'):
+            self._app.add_content(flag)
+        elif flag == 'SEND':
+            # result, text_error = create_issue(
+            #    {'file': None, 'image': None, 'issue': 'Text message...',
+            #     'theme': ''}
+            # )
+            # print(result, text_error)
+            pass
