101af4230d7fade8f81a66644e2f4662ce0ccdd9
CityDoomed <gorodage@gmail.com>
2016-12-31 22:09:54 +0300

    Отправка вопроса в группу вынесена в файл program.py в функцию callback_for_input_text

--- libs/uix/kv/activity/baseclass/previous.py
+++ libs/uix/kv/activity/baseclass/previous.py
@@ -3,8 +3,6 @@
 from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty
 
-from libs.vkrequests import create_issue
-
 
 class Previous(Screen):
     
@@ -17,15 +15,4 @@ class Previous(Screen):
             [['menu', lambda x: self._app.nav_drawer.toggle()]]
         self._app.screen.ids.action_bar.title = self._app.title
 
-        self.ids.input_text_form.callback = self.callback_for_input_text
-
-    def callback_for_input_text(self, flag):
-        if flag in ('FILE', 'FOTO'):
-            self._app.add_content(flag)
-        elif flag == 'SEND':
-            # result, text_error = create_issue(
-            #    {'file': None, 'image': None, 'issue': 'Text message...',
-            #     'theme': ''}
-            # )
-            # print(result, text_error)
-            pass
+        self.ids.input_text_form.callback = self._app.callback_for_input_text
