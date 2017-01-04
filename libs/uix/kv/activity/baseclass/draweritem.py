a93387d0a7bae4f6aa1f70522737c89795f5dc6e
Юрий <HeaTTheatR@users.noreply.github.com>
2017-01-05 01:10:44 +0400

    Delete draweritem.py

--- libs/uix/kv/activity/baseclass/draweritem.py
+++
@@ -1,8 +0,0 @@
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty, ObjectProperty
-
-
-class DrawerItem(BoxLayout):
-    icon = StringProperty('vector-circle')
-    text = StringProperty()
-    callback = ObjectProperty(lambda: None)
