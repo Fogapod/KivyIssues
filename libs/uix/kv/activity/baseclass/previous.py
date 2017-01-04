dc3352c6bb487e13bdb102121f09b5f2fcfaeb72
CityDoomed <gorodage@gmail.com>
2017-01-05 00:00:18 +0300

    Переименовал вызов метода toggle на _toggle

--- libs/uix/kv/activity/baseclass/previous.py
+++ libs/uix/kv/activity/baseclass/previous.py
@@ -12,7 +12,7 @@ class Previous(Screen):
         '''Вызывается при установке Activity на экран.'''
 
         self._app.screen.ids.action_bar.left_action_items = \
-            [['menu', lambda x: self._app.nav_drawer.toggle()]]
+            [['menu', lambda x: self._app.nav_drawer._toggle()]]
         self._app.screen.ids.action_bar.title = self._app.title
 
         self.ids.input_text_form.callback = self._app.callback_for_input_text
