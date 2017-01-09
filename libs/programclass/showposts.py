f54c99a9c2e5fabd298aae1dcaaaf096a8e5df3b
CityDoomed <gorodage@gmail.com>
2017-01-09 20:21:03 +0300

    Использование нового прогресс бара

--- libs/programclass/showposts.py
+++ libs/programclass/showposts.py
@@ -4,8 +4,9 @@
 # и управляет созданием экранов с полученными данными.
 #
 
-from kivy.clock import Clock
+from kivy.uix.image import Image
 from kivy.uix.screenmanager import Screen
+from kivy.clock import Clock
 
 from libs.programdata import thread
 
@@ -44,6 +45,8 @@ class ShowPosts(object):
         else:
             self.commented_post_info = commented_post_info
 
+        self.set_chevron_back_screen()
+
     def set_chevron_back_screen(self):
         '''Устанавливает шеврон возврата к предыдущему экрану в ToolBar.'''
 
@@ -53,7 +56,7 @@ class ShowPosts(object):
                 [['comment-outline', lambda x: None]]
         self._app.screen.ids.action_bar.left_action_items = \
             [['chevron-left', lambda x: self._app.back_screen(
-                self.previous_screen)]]
+                self._app.manager.current)]]
 
     @thread
     def _set_info_for_post(self):
@@ -86,18 +89,21 @@ class ShowPosts(object):
                     on_ref_press=lambda *args: self.jump_to_page(args)
                 )
 
-                #self.previous_screen = self._app.manager.current
-                #self.set_chevron_back_screen()
-
                 screen = Screen(name=name_screen)
                 screen.add_widget(box_posts)
                 self._app.manager.add_widget(screen)
                 self._app.manager.current = name_screen
 
                 Clock.unschedule(check_posts_dict)
-                self._app.dialog_progress.dismiss()
+                # self._app.dialog_progress.dismiss()
+                self._app.screen.ids.action_bar.title = \
+                    self._app.data.string_lang_page + \
+                    str(self.current_number_page)
 
-        self._app.show_progress(text=self._app.data.string_lang_wait)
+        progress_line = Image(source='data/images/waiting.gif',
+                              size_hint_y=None, pos=(0, -35))
+        self._app.screen.ids.previous.ids.box.add_widget(progress_line)
+        # self._app.show_progress(text=self._app.data.string_lang_wait)
 
         if not self._app.manager.has_screen(name_screen):
             self._set_info_for_post()
@@ -120,11 +126,9 @@ class ShowPosts(object):
 
     def show_posts(self):
         if not self.comments:
-            name_screen = \
-                'box posts page ' + str(self.current_number_page)
+            name_screen = 'box posts page ' + str(self.current_number_page)
         else:
-            name_screen = \
-                'box comments page ' + str(self.current_number_page)
+            name_screen = 'box comments page ' + str(self.current_number_page)
 
         if self._app.manager.has_screen(name_screen):
             self._app.manager.current = name_screen
