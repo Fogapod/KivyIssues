9bc5305f3335dd70ad48675360d2e0a1979fc32b
CityDoomed <gorodage@gmail.com>
2017-01-12 13:54:59 +0300

    initial commit

--- libs/uix/kv/activity/baseclass/license.py
+++ libs/uix/kv/activity/baseclass/license.py
@@ -1,18 +1,5 @@
-# -*- coding: utf-8 -*-
-#
-# Выводит экран с текстом лицензии.
-#
-
-import os
-
 from kivy.uix.screenmanager import Screen
-from kivy.properties import ObjectProperty
 
-from libs.uix.dialogs import dialog
-libs/uix/kv/activity/baseclass/license.py
 
 class ShowLicense(Screen):
-
-    _app = ObjectProperty()
-
-
+    pass
